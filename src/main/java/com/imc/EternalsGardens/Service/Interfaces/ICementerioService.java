b742605b63eeacbf7d1a65a5454365c10f59e38d
Izanmc18 <165163853+Izanmc18@users.noreply.github.com>
2026-03-03 23:18:34 +0100

    Fin proyecto

--- src/main/java/com/imc/EternalsGardens/Service/Interfaces/ICementerioService.java
+++ src/main/java/com/imc/EternalsGardens/Service/Interfaces/ICementerioService.java
@@ -2,31 +2,30 @@ package com.imc.EternalsGardens.Service.Interfaces;
 
 import com.imc.EternalsGardens.DTO.Request.CementerioRequest;
 import com.imc.EternalsGardens.DTO.Response.CementerioResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface ICementerioService {
 
-    CementerioResponse crearCementerio(CementerioRequest request);
+        CementerioResponse crearCementerio(CementerioRequest request);
 
-    List<CementerioResponse> obtenerTodos();
+        List<CementerioResponse> obtenerTodos();
 
-    org.springframework.data.domain.Page<CementerioResponse> obtenerTodos(
-            org.springframework.data.domain.Pageable pageable);
+        Page<CementerioResponse> obtenerTodos(Pageable pageable);
 
-    org.springframework.data.domain.Page<CementerioResponse> obtenerTodos(
-            org.springframework.data.domain.Pageable pageable,
-            String provincia);
+        Page<CementerioResponse> obtenerTodos(Pageable pageable, String provincia);
 
-    CementerioResponse obtenerPorId(Integer id);
+        CementerioResponse obtenerPorId(Integer id);
 
-    CementerioResponse actualizarCementerio(Integer id, CementerioRequest request);
+        CementerioResponse actualizarCementerio(Integer id, CementerioRequest request);
 
-    void eliminarCementerio(Integer id); // Borrado lógico
+        void eliminarCementerio(Integer id); // Borrado lógico
 
-    List<CementerioResponse> buscarPorLocalizacion(String busqueda);
+        List<CementerioResponse> buscarPorLocalizacion(String busqueda);
 
-    @Transactional(readOnly = true)
-    List<CementerioResponse> buscarPorNombreOLocalizacion(String busqueda);
+        @Transactional(readOnly = true)
+        List<CementerioResponse> buscarPorNombreOLocalizacion(String busqueda);
 }
